3f6ed5ea48cd0f73de1f2f4287f9a82e83aba312
khushalpt94 <60636789+khushalpt94@users.noreply.github.com>
2020-02-28 15:09:19 -0500

    Add files via upload

---
+++ sparsify.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pandas as pd
+import numpy as np
+
+from tools.generic import gigs, one_hot, to_sparse
+
+# Reading in the data
+slim_cols = [COLUMNS_TO_USE]
+records = pd.read_csv(EHR_CSV_FILE, usecols=slim_cols)
+
+# Making the sparse matrices
+sparse_out = [to_sparse(records[col].astype(str)) for col in slim_cols]
+sparse_csr = hstack([col['data'] for col in sparse_out], format='csr')
+sparse_vocab = [col['vocab'] for col in sparse_out]
+sparse_vocab = pd.Series([item for sublist in sparse_vocab
+                          for item in sublist])
+
+# Writing the files to disk
+save_npz('sparse_records.npz', sparse_csr)
+sparse_vocab.to_csv('sparse_vocab.csv', index=False)
+
